88a39909ff4188301385efef1833363d7fbbcf51
David Del Valle <dvallepuado@gmail.com>
2025-01-30 11:59:21 +0100

    Test 4

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,32 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginVue from "eslint-plugin-vue";
 
-
 /** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,vue}"]},
-  {languageOptions: { globals: globals.browser }},
+const eslintConfig = [
+  {
+    files: ["**/*.{js,mjs,cjs,vue}"],
+    languageOptions: { globals: globals.browser },
+  },
   pluginJs.configs.recommended,
   ...pluginVue.configs["flat/essential"],
+  {
+    overrides: [
+      {
+        files: ["*.jsx"],
+        parserOptions: {
+          ecmaVersion: 12,
+          sourceType: "module",
+        },
+        rules: {
+          // Aquí puedes definir reglas adicionales para JSX
+        },
+      },
+    ],
+  },
+  // Esto es para asegurarte de que ESLint escanee el directorio correcto
+  {
+    root: true,
+  },
 ];
+
+export default eslintConfig;
